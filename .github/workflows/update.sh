6f4d0c463a8cd1e1a279168b52af7cae81953aeb
miidas <67558797+miidas0@users.noreply.github.com>
2020-06-30 13:13:48 +0900

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Install hugo
-sudo apt-get install hugo
+wget https://github.com/gohugoio/hugo/releases/download/v0.73.0/hugo_extended_0.73.0_Linux-64bit.deb
+sudo apt install ./hugo_extended_0.73.0_Linux-64bit.deb
 
 # Print hugo version 
 hugo version
@@ -8,7 +9,9 @@ hugo version
 # Generate static files
 hugo
 
+# Show generated files
 ls -la public/
+
 #git config --global user.name "miidas"
 #git config --global user.email "67558797+miidas0@users.noreply.github.com"
 #git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.blog.git
