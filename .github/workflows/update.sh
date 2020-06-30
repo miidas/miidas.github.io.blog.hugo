4449a6671b916a43613e3abd507b5fc0ba0f351e
miidas <67558797+miidas0@users.noreply.github.com>
2020-06-30 13:30:01 +0900

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -4,26 +4,15 @@ wget -q https://github.com/gohugoio/hugo/releases/download/v0.73.0/hugo_extended
 sudo apt install ./hugo_extended_0.73.0_Linux-64bit.deb
 rm -f hugo_extended_0.73.0_Linux-64bit.deb
 
-# 
-# Print hugo version 
-hugo version
-
 # Generate static files
 hugo
 
-# Debug
-ls -la
-
-# Show generated files
-ls -la public/
-
-#git config --global user.name "miidas"
-#git config --global user.email "67558797+miidas0@users.noreply.github.com"
-#git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.blog.git
-#cd ${GITHUB_ACTOR}.github.io
-#cd blog
-#git checkout master
-#cd ../
-#git add blog
-#git commit -m "[CI] Update blog submodule"
-#git push
+# Push to the blog repository
+git config --global user.name "miidas"
+git config --global user.email "67558797+miidas0@users.noreply.github.com"
+git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.blog.git
+cd ${GITHUB_ACTOR}.github.io.blog
+mv ../public/* ./
+git add --all
+git commit -m "[CI] Rebuild the website"
+git push
