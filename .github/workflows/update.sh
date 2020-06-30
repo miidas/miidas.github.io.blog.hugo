1db16caf52f121693f72698d93bca505cc6c887f
miidas <67558797+miidas0@users.noreply.github.com>
2020-06-30 14:29:25 +0900

    Update update.sh

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -12,7 +12,7 @@ git config --global user.name "miidas"
 git config --global user.email "67558797+miidas0@users.noreply.github.com"
 git clone --recursive https://${GITHUB_ACTOR}:${_GITHUB_ACCESS_TOKEN_}@github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.github.io.blog.git
 cd ${GITHUB_ACTOR}.github.io.blog
-mv -f ../public/* ./
+cp -r ../public/* ./
 git add --all
 git commit -m "[CI] Rebuild the website"
 git push
